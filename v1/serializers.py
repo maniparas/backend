4b5a7316e303327694077e7c56147d5ad7a64348
Paras Mani <maniparas15@gmail.com>
2017-11-14 22:41:45 +0530

    reformatted serializers code

--- v1/serializers.py
+++ v1/serializers.py
@@ -1,18 +1,20 @@
 from rest_framework import serializers
 from .models import Case, UserProfile, Sighting
 
+
 class CaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Case
         fields = '__all__'
 
+
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = '__all__'        
+        fields = '__all__'
+
 
 class SightingSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Sighting 
-        fields = ('case','sighted_by','location_sighted','date_sighted','additional_info')
-
+        model = Sighting
+        fields = ('case', 'sighted_by', 'location_sighted', 'date_sighted', 'additional_info')
